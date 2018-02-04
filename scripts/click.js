053b147612b6ca2315583dd8a491480e854cdd8b
olaven <olavsundfoer@gmail.com>
2018-02-04 14:12:46 +0100

    new page

--- scripts/click.js
+++
@@ -1,8 +0,0 @@
-//this function handles all click events on the document
-let click = (evt)=>{
-  /*click on circles*/
-  if (evt.target.className === "circles") {
-    // IDEA: circles "drop down" and bounce up again
-    //evt.target.style.
-  }
-}
