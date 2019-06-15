ae787d3142865ffc513fcfb69668df6df2382de2
olaven <olavsundfoer@gmail.com>
2019-06-16 00:08:28 +0200

    achieved a setup I am decently happy with. Using MDX!

--- README.md
+++
@@ -1,6 +0,0 @@
-# [olaven.org](https://olaven.org)
-My personal website.
-
-
-- [ ] Contact information 
-- [ ] link to blog at /blog
---
+++ next.config.js
@@ -0,0 +1,8 @@
+const withTypescript = require('@zeit/next-typescript'); 
+const withMDX = require('@next/mdx')()
+
+module.exports = withTypescript(
+    withMDX({
+        pageExtensions: ['ts', 'tsx', 'md', 'mdx']
+    })
+); 
